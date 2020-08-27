bfa2881366be952504351003b88fa2ce48b31181
Diogo Figueiredo <diogo.alvesfigueiredo@gmail.com>
2020-08-27 16:37:25 +0100

    edit and remove added

--- app/src/main/java/com/example/storedatakotlin/MainActivity.kt
+++ app/src/main/java/com/example/storedatakotlin/MainActivity.kt
@@ -10,13 +10,27 @@ class MainActivity : AppCompatActivity() {
 
         val sharedPreferences = this.getSharedPreferences("com.example.storedatakotlin", android.content.Context.MODE_PRIVATE)
 
-        var age = 30
+        val age = 30
 
-        sharedPreferences.edit().putInt("userAge", age).apply()
+//        sharedPreferences.edit().putInt("userAge", age).apply()
 
         val storedAge = sharedPreferences.getInt("userAge", 0)
 
         println("stored age: $storedAge")
+
+        age = 31
+
+//        sharedPreferences.edit().putInt("userAge", age).apply()
+
+        val storedAge2 = sharedPreferences.getInt("userAge", 0)
+
+        println("stored age 2: $storedAge2")
+
+//        sharedPreferences.edit().remove("userAge").apply()
+
+        val storedAge3 = sharedPreferences.getInt("userAge", 0)
+
+        println("stored age 3: $storedAge3")
 
 
     }
